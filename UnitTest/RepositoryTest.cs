da4136c398d3f63285b2ce53508fe344d04e45af
KimKyengJun <juniair.kim@gmail.com>
2018-06-08 13:51:36 +0900

    Test 모듈 구현

---
+++ UnitTest/RepositoryTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class RepositoryTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
